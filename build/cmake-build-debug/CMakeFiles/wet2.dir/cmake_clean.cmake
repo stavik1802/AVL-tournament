2206086184b02f59f725a2dde1d8144ec32b464f
stavik1802 <stavos114@gmail.com>
2025-09-04 15:17:40 +0300

    Add files via upload

---
+++ build/cmake-build-debug/CMakeFiles/wet2.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/wet2.dir/Team.cpp.obj"
+  "CMakeFiles/wet2.dir/main24a2.cpp.obj"
+  "CMakeFiles/wet2.dir/olympics24a2.cpp.obj"
+  "libwet2.dll.a"
+  "wet2.exe"
+  "wet2.exe.manifest"
+  "wet2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/wet2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
